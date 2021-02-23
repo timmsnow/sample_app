030c9a20bb4f4642d6fc9cae0172736ae2c14eab
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-23 10:58:59 -0500

    sets up json.jb views, experiements with html

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,13 +1,27 @@
 class Api::ExamplePagesController < ApplicationController
   def nunya_action
-    render json: { message: "NUNYA BUSINESS!" }
+    # render json: { message: "nunya business" }
+
+    @message = "nunya business"
+
+    render "nunya.json.jb"
   end
 
   def hippo_time
-    render json: { alert: "HIPPOS CAN'T SWIM" }
+    @message = "HIPPOS CANT FUCKING SWIM"
+    render "hippo.json.jb"
   end
 
   def giraffe_time
-    render inline: "<h1> Hello</h1> <img src='https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_giraffe_1_0.jpg'>"
+    render "giraffe.html"
   end
 end
+
+#render without .json.jb
+# def hippo_time
+#   render json: { alert: "HIPPOS CAN'T SWIM" }
+# end
+
+# def giraffe_time
+#   render inline: "<h1> Hello</h1> <img src='https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_giraffe_1_0.jpg' width='500px'>"
+# end
